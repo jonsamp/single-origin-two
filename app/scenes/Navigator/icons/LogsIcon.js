6a8952c2a2787e511bac6a4beff1d84bf313a432
jonsamp <jon@codecademy.com>
2019-02-07 00:48:02 -0500

    log icon appears larger

--- app/scenes/Navigator/icons/LogsIcon.js
+++ app/scenes/Navigator/icons/LogsIcon.js
@@ -15,13 +15,13 @@ const propTypes = {
 };
 
 const LogsIcon = ({ focused, theme }) => (
-  <Svg width="28" height="24">
+  <Svg width="28" height="27">
     <Svg.G
       fill={focused ? themes[theme].primary : themes[theme].foreground}
       fillRule="nonzero"
     >
-      <Svg.Path d="M10.584 8.71a1 1 0 1 1 0-2h7.244a1 1 0 0 1 0 2h-7.244zM6.453 18.73a1 1 0 0 1 0-2h15.016a1 1 0 1 1 0 2H6.453zM6.61 14.897a1 1 0 1 1 0-2h14.885a1 1 0 1 1 0 2H6.61z" />
-      <Svg.Path d="M4.552 2.816a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h19a2 2 0 0 0 2-2v-15a2 2 0 0 0-2-2h-19zm0-2h19a4 4 0 0 1 4 4v15a4 4 0 0 1-4 4h-19a4 4 0 0 1-4-4v-15a4 4 0 0 1 4-4z" />
+      <Svg.Path d="M10.568 8.21a1 1 0 1 1 0-2h7.245a1 1 0 0 1 0 2h-7.245zM6.437 18.23a1 1 0 0 1 0-2h15.017a1 1 0 1 1 0 2H6.437zM6.594 14.397a1 1 0 1 1 0-2H21.48a1 1 0 1 1 0 2H6.594z" />
+      <Svg.Path d="M4.537 2.316a2 2 0 0 0-2 2v18a2 2 0 0 0 2 2h19a2 2 0 0 0 2-2v-18a2 2 0 0 0-2-2h-19zm0-2h19a4 4 0 0 1 4 4v18a4 4 0 0 1-4 4h-19a4 4 0 0 1-4-4v-18a4 4 0 0 1 4-4z" />
     </Svg.G>
   </Svg>
 );
