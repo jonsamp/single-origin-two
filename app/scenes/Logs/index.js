cda3512a5deb96e82f60afecbc242327f1e05176
jonsamp <jon@codecademy.com>
2019-02-07 01:39:16 -0500

    adds calendars package

--- app/scenes/Logs/index.js
+++ app/scenes/Logs/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, ScrollView, Text } from 'react-native';
 import withTheme from 'providers/theme';
 import type from 'constants/type';
-import HeaderScrollView from 'react-native-header-scroll-view';
+import Calendar from './Calendar';
 
 const mapStateToProps = state => ({});
 
@@ -18,74 +18,11 @@ class Logs extends Component {
   render() {
     const { theme } = this.props;
     return (
-      <HeaderScrollView title="For You">
-        <Text style={[type.body, { padding: 16 }]}>
-          Between them and the beasts behind me there was little choice, but at
-          least there was a doubt as to the reception these grotesque parodies
-          on humanity would accord me, while there was none as to the fate which
-          awaited me beneath the grinning fangs of my fierce pursuers. And so I
-          raced on toward the trees intending to pass beneath that which held
-          the man-things and take refuge in another farther on; but the
-          wolf-dogs were very close behind me—so close that I had despaired of
-          escaping them, when one of the creatures in the tree above swung down
-          Between them and the beasts behind me there was little choice, but at
-          least there was a doubt as to the reception these grotesque parodies
-          on humanity would accord me, while there was none as to the fate which
-          awaited me beneath the grinning fangs of my fierce pursuers. And so I
-          raced on toward the trees intending to pass beneath that which held
-          the man-things and take refuge in another farther on; but the
-          wolf-dogs were very close behind me—so close that I had despaired of
-          escaping them, when one of the creatures in the tree above swung down
-          Between them and the beasts behind me there was little choice, but at
-          least there was a doubt as to the reception these grotesque parodies
-          on humanity would accord me, while there was none as to the fate which
-          awaited me beneath the grinning fangs of my fierce pursuers. And so I
-          raced on toward the trees intending to pass beneath that which held
-          the man-things and take refuge in another farther on; but the
-          wolf-dogs were very close behind me—so close that I had despaired of
-          escaping them, when one of the creatures in the tree above swung down
-          Between them and the beasts behind me there was little choice, but at
-          least there was a doubt as to the reception these grotesque parodies
-          on humanity would accord me, while there was none as to the fate which
-          awaited me beneath the grinning fangs of my fierce pursuers. And so I
-          raced on toward the trees intending to pass beneath that which held
-          the man-things and take refuge in another farther on; but the
-          wolf-dogs were very close behind me—so close that I had despaired of
-          escaping them, when one of the creatures in the tree above swung down
-          Between them and the beasts behind me there was little choice, but at
-          least there was a doubt as to the reception these grotesque parodies
-          on humanity would accord me, while there was none as to the fate which
-          awaited me beneath the grinning fangs of my fierce pursuers. And so I
-          raced on toward the trees intending to pass beneath that which held
-          the man-things and take refuge in another farther on; but the
-          wolf-dogs were very close behind me—so close that I had despaired of
-          escaping them, when one of the creatures in the tree above swung down
-          Between them and the beasts behind me there was little choice, but at
-          least there was a doubt as to the reception these grotesque parodies
-          on humanity would accord me, while there was none as to the fate which
-          awaited me beneath the grinning fangs of my fierce pursuers. And so I
-          raced on toward the trees intending to pass beneath that which held
-          the man-things and take refuge in another farther on; but the
-          wolf-dogs were very close behind me—so close that I had despaired of
-          escaping them, when one of the creatures in the tree above swung down
-          Between them and the beasts behind me there was little choice, but at
-          least there was a doubt as to the reception these grotesque parodies
-          on humanity would accord me, while there was none as to the fate which
-          awaited me beneath the grinning fangs of my fierce pursuers. And so I
-          raced on toward the trees intending to pass beneath that which held
-          the man-things and take refuge in another farther on; but the
-          wolf-dogs were very close behind me—so close that I had despaired of
-          escaping them, when one of the creatures in the tree above swung down
-          Between them and the beasts behind me there was little choice, but at
-          least there was a doubt as to the reception these grotesque parodies
-          on humanity would accord me, while there was none as to the fate which
-          awaited me beneath the grinning fangs of my fierce pursuers. And so I
-          raced on toward the trees intending to pass beneath that which held
-          the man-things and take refuge in another farther on; but the
-          wolf-dogs were very close behind me—so close that I had despaired of
-          escaping them, when one of the creatures in the tree above swung down
-        </Text>
-      </HeaderScrollView>
+      <View style={{ flex: 1, backgroundColor: theme.background }}>
+        <ScrollView>
+          <Calendar />
+        </ScrollView>
+      </View>
     );
   }
 }
