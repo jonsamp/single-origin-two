4241174d460451e64094702b1e0a8f18c6b13f1e
jonsamp <jon@codecademy.com>
2019-06-07 22:53:19 -0400

    updates svgs

--- src/app/components/Image/icons/PauseIcon.tsx
+++ src/app/components/Image/icons/PauseIcon.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import * as Svg from 'react-native-svg';
+import { Svg, G, Rect } from 'react-native-svg';
 
 function PauseIcon() {
   return (
     <Svg width="13" height="15">
-      <Svg.G transform="translate(.45 .385)" fill="#FFF" fillRule="evenodd">
-        <Svg.Rect width="4.707" height="14.363" rx="1" />
-        <Svg.Rect x="7.481" width="4.707" height="14.363" rx="1" />
-      </Svg.G>
+      <G transform="translate(.45 .385)" fill="#FFF" fillRule="evenodd">
+        <Rect width="4.707" height="14.363" rx="1" />
+        <Rect x="7.481" width="4.707" height="14.363" rx="1" />
+      </G>
     </Svg>
   )
 }
--- src/app/components/Instructions/icons/WaterIcon.tsx
+++ src/app/components/Instructions/icons/WaterIcon.tsx
@@ -1,11 +1,11 @@
-import * as Svg from 'react-native-svg';
 import React from 'react'
+import { Path, Svg } from 'react-native-svg'
 import { Icon } from './types'
 
 function WaterIcon({ fill }: Icon) {
   return (
     <Svg width="16" height="22">
-      <Svg.Path
+      <Path
         d="M14.702 11.132L9.174.872a1.106 1.106 0 0 0-1.946 0L1.7 11.132a7.385 7.385 0 1 0 13.002 0z"
         fill={fill}
         fillRule="evenodd"
