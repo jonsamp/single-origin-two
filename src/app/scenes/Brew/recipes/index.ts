73d8e1a37505355ec8ac9ef62a639e40a2e9513a
jonsamp <sampjon@gmail.com>
2019-09-15 15:08:17 -0400

    adds 3 cup chemex

--- src/app/scenes/Brew/recipes/index.ts
+++ src/app/scenes/Brew/recipes/index.ts
@@ -1,6 +1,7 @@
 import Chemex from './Chemex'
+import Chemex3Cup from './Chemex3Cup'
 import Clever from './Clever'
 import IcedKalitaWave185 from './IcedKalitaWave185'
 import KalitaWave185 from './KalitaWave185'
 
-export default { Chemex, Clever, KalitaWave185, IcedKalitaWave185 }
+export default { Chemex, Chemex3Cup, Clever, KalitaWave185, IcedKalitaWave185 }
