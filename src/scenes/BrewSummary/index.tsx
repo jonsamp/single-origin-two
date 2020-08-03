f258554eadbbf922b0f3e6471e1c5b9115385030
jonsamp <sampjon@gmail.com>
2020-08-03 19:15:02 -0400

    use pop to top from native stack

--- src/scenes/BrewSummary/index.tsx
+++ src/scenes/BrewSummary/index.tsx
@@ -1,8 +1,6 @@
 import * as StoreReview from 'expo-store-review'
 import React, { useEffect, useLayoutEffect } from 'react'
 import { View } from 'react-native'
-import { NavigationScreenProp } from 'react-navigation'
-import { StackActions } from '@react-navigation/native'
 import { connect } from 'react-redux'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Button from '../../components/Button'
@@ -10,17 +8,17 @@ import { HeaderBackButton } from '@react-navigation/stack'
 import Log from '../../components/Log'
 import { width } from '../../constants/layout'
 import withSettings, { Settings } from '../../providers/settings'
-import withTheme, { Styleguide } from '../../providers/theme'
+import withTheme, { Styleguide, Theme } from '../../providers/theme'
 import { selectLog } from '../../state/logs/selectors'
-import { State } from '../../state/types'
 import styles from './styles'
 
 interface BrewSummaryProps {
-  navigation: NavigationScreenProp<State, any>
+  navigation: any
   settings: Settings
   settingUpdated: (props: { setting: string; value: any }) => void
   styleguide: Styleguide
   route: any
+  theme: Theme
 }
 
 const mapStateToProps = (state, props) => {
@@ -30,7 +28,7 @@ const mapStateToProps = (state, props) => {
   }
 }
 
-function BrewSummary(props: Props) {
+function BrewSummary(props: BrewSummaryProps) {
   const {
     settings,
     settingUpdated,
@@ -40,7 +38,7 @@ function BrewSummary(props: Props) {
     theme,
   } = props
 
-  const onBack = () => navigation.dispatch(StackActions.popToTop())
+  const onBack = () => navigation.popToTop()
 
   useEffect(function didMount() {
     async function getStoreReview() {
