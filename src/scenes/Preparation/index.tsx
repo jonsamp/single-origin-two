ac586cc1c8749bf733eaddba055e43b15dc79c43
jonsamp <sampjon@gmail.com>
2020-08-14 08:20:53 -0400

    Capture sentry message

--- src/scenes/Preparation/index.tsx
+++ src/scenes/Preparation/index.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import * as Sentry from 'sentry-expo'
+import { isArray } from 'lodash'
 import InstructionalCard from '../../components/InstructionalCard'
 import ResponsiveScrollView from '../../components/ResponsiveScrollView'
 import withTheme, { Theme } from '../../providers/theme'
@@ -18,6 +20,13 @@ class Preparation extends Component<PreparationProps> {
   render() {
     const { route, theme, isDarkTheme } = this.props
     const preparation = route.params
+    const prepSteps = isArray(preparation) ? preparation : []
+
+    if (!isArray(preparation)) {
+      Sentry.captureMessage(
+        `Preparation was not an array. It was: ${JSON.stringify(preparation)}.`
+      )
+    }
 
     return (
       <ResponsiveScrollView
@@ -25,7 +34,7 @@ class Preparation extends Component<PreparationProps> {
           backgroundColor: isDarkTheme ? theme.background : theme.grey1,
         }}
       >
-        {preparation.map(prepStep => (
+        {prepSteps.map(prepStep => (
           <InstructionalCard
             key={prepStep.text}
             step={{ image: prepStep.image, description: prepStep.text }}
