66fa1a1d09175c6917237e61795bd0a257f25a6c
jonsamp <sampjon@gmail.com>
2020-09-12 16:38:01 -0400

    Revert "use hook for loading screen"
    This reverts commit c5553a5fc2327ead23379c6c4a90b285b878f7c7.

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
+import { SplashScreen } from 'expo'
 import * as ScreenOrientation from 'expo-screen-orientation'
 import Constants from 'expo-constants'
-import React, { useEffect } from 'react'
-import { ActivityIndicator, View } from 'react-native'
+import React, { Component } from 'react'
+import { ActivityIndicator, View, Animated } from 'react-native'
 import { AppearanceProvider } from 'react-native-appearance'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import * as Sentry from 'sentry-expo'
+import * as Font from 'expo-font'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-
 import Navigator from './src/navigation'
 import configureStore from './src/store/configureStore'
 import { enableScreens } from 'react-native-screens'
-import { useCachedResources } from './src/hooks/useCachedResources'
 
 enableScreens()
 
@@ -24,51 +24,138 @@ Sentry.init({
 
 Sentry.setRelease(Constants.manifest.revisionId)
 
-export default function App() {
-  useEffect(function didMount() {
-    async function modifyOrientation() {
-      if (
-        Constants.platform &&
-        Constants.platform.ios &&
-        Constants.platform.ios.userInterfaceIdiom === 'tablet'
-      ) {
-        await ScreenOrientation.lockAsync(
-          ScreenOrientation.OrientationLock.DEFAULT
-        )
-      }
-    }
+let customFonts = {
+  Script: require('./assets/SignPainter-HouseScript.ttf'),
+}
 
-    modifyOrientation()
-  }, [])
+type State = {
+  splashAnimationComplete: boolean
+  splashAnimation: Animated.Value
+  fontsLoaded: boolean
+}
 
-  const isLoadingComplete = useCachedResources()
-  if (!isLoadingComplete) {
-    return null
+class App extends Component<null, State> {
+  state = {
+    splashAnimationComplete: false,
+    splashAnimation: new Animated.Value(0),
+    fontsLoaded: false,
   }
 
-  return (
-    <Provider store={store}>
-      <PersistGate
-        loading={
-          <View
-            style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: 'black',
-            }}
-          >
-            <ActivityIndicator />
-          </View>
-        }
-        persistor={persistor}
+  async componentDidMount() {
+    SplashScreen.preventAutoHide()
+
+    this._loadFontsAsync()
+
+    if (
+      Constants.platform &&
+      Constants.platform.ios &&
+      Constants.platform.ios.userInterfaceIdiom === 'tablet'
+    ) {
+      await ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.DEFAULT
+      )
+    }
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts)
+    this.setState({ fontsLoaded: true })
+  }
+
+  _maybeRenderLoadingImage = () => {
+    if (this.state.splashAnimationComplete) {
+      return null
+    }
+
+    return (
+      <Animated.View
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#000',
+          opacity: this.state.splashAnimation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [1, 0],
+          }),
+          transform: [
+            {
+              scale: this.state.splashAnimation.interpolate({
+                inputRange: [0, 1],
+                outputRange: [1, 1.5],
+              }),
+            },
+            {
+              translateY: this.state.splashAnimation.interpolate({
+                inputRange: [0, 1],
+                outputRange: [1, 4],
+              }),
+            },
+          ],
+        }}
       >
-        <SafeAreaProvider>
-          <AppearanceProvider>
-            <Navigator />
-          </AppearanceProvider>
-        </SafeAreaProvider>
-      </PersistGate>
-    </Provider>
-  )
+        <Animated.Image
+          source={require('./assets/splash.png')}
+          style={{
+            width: undefined,
+            height: undefined,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            resizeMode: 'contain',
+          }}
+          onLoadEnd={this._animateOut}
+        />
+      </Animated.View>
+    )
+  }
+
+  _animateOut = () => {
+    SplashScreen.hide()
+    Animated.timing(this.state.splashAnimation, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start(() => {
+      this.setState({ splashAnimationComplete: true })
+    })
+  }
+
+  render() {
+    if (!this.state.fontsLoaded) return null
+    return (
+      <SafeAreaProvider>
+        <Provider store={store}>
+          <PersistGate
+            loading={
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'black',
+                }}
+              >
+                <ActivityIndicator />
+              </View>
+            }
+            persistor={persistor}
+          >
+            <AppearanceProvider>
+              <Navigator />
+              {this._maybeRenderLoadingImage()}
+            </AppearanceProvider>
+          </PersistGate>
+        </Provider>
+      </SafeAreaProvider>
+    )
+  }
 }
+
+export default App
