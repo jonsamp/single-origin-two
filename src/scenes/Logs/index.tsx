e56a8ebb72b02440995ca6666a1ff5353a8c2a38
jonsamp <sampjon@gmail.com>
2020-12-16 11:26:58 -0500

    convert to function component

--- src/scenes/Logs/index.tsx
+++ src/scenes/Logs/index.tsx
@@ -1,69 +1,63 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { FlatList, View } from 'react-native'
 import { connect } from 'react-redux'
 import ScreenPlaceholder from '../../components/ScreenPlaceholder'
 import recipes from '../../constants/recipes'
-import withTheme from '../../providers/theme'
+import { useTheme } from '../../providers/theme'
 import { selectLogs } from '../../state/logs/selectors'
-import { Logs as LogsType, Theme } from '../../types/index'
+import { Logs as LogsType, Log } from '../../types/index'
 import LogItem from './LogItem'
 import styles from './styles'
 
-interface LogsProps {
-  theme: Theme
+interface Props {
   logs: LogsType
-  isDarkTheme: boolean
   navigation: any
 }
 
-interface LogsState {
-  editing: boolean
-}
-
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   logs: selectLogs(state),
 })
 
-class Logs extends Component<LogsProps, LogsState> {
-  byTimestamp = (a, b) => b.timestamp - a.timestamp
+function Logs(props: Props) {
+  const { logs, navigation } = props
+  const { colors } = useTheme()
 
-  render() {
-    const { theme, logs, navigation } = this.props
-
-    return (
-      <FlatList
-        contentContainerStyle={{ paddingTop: 16 }}
-        data={Object.values(logs)
-          .filter(log => log && recipes[log.recipeId])
-          .sort(this.byTimestamp)}
-        keyExtractor={log => String(log.timestamp)}
-        renderItem={({ item }) => (
-          <LogItem
-            log={item}
-            onPress={() =>
-              navigation.navigate('LogDetail', {
-                timestamp: item.timestamp,
-              })
-            }
-          />
-        )}
-        extraData={this.state}
-        ItemSeparatorComponent={() => (
-          <View
-            style={[
-              styles.separator,
-              {
-                backgroundColor: theme.border,
-              },
-            ]}
-          />
-        )}
-        ListEmptyComponent={
-          <ScreenPlaceholder text="Notes of each brew will appear here once you complete a brew." />
-        }
-      />
-    )
+  function byTimestamp(a: Log, b: Log) {
+    return b.timestamp - a.timestamp
   }
+
+  return (
+    <FlatList
+      contentContainerStyle={{ paddingTop: 16 }}
+      data={Object.values(logs)
+        .filter((log) => log && recipes[log.recipeId])
+        .sort(byTimestamp)}
+      keyExtractor={(log) => String(log.timestamp)}
+      renderItem={({ item }) => (
+        <LogItem
+          log={item}
+          onPress={() =>
+            navigation.navigate('LogDetail', {
+              timestamp: item.timestamp,
+            })
+          }
+        />
+      )}
+      ItemSeparatorComponent={() => (
+        <View
+          style={[
+            styles.separator,
+            {
+              backgroundColor: colors.border,
+            },
+          ]}
+        />
+      )}
+      ListEmptyComponent={
+        <ScreenPlaceholder text="Notes of each brew will appear here once you complete a brew." />
+      }
+    />
+  )
 }
 
-export default connect(mapStateToProps)(withTheme(Logs))
+export default connect(mapStateToProps)(Logs)
